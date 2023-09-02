3d67df2dfed68d67b27951992066eaf08bcb98f6
Allonte <angelosheltie@gmail.com>
2023-09-02 10:00:22 -0700

    Update test_portal1.py

--- examples/test_portal1.py
+++ examples/test_portal1.py
@@ -6,17 +6,17 @@ gym.register("portal1", "dungeonasium.envs.portal_worlds:PortalWorldEnv")
 
 if __name__ == "__main__":
     env1 = gym.make_vec("portal1", render_mode=None, num_envs=10)
-    env2 = gym.make("portal1", render_mode="human")
+    env2 = gym.make_vec("portal1", render_mode="human")
 
     # Create the Q-learning agent instance
     agent = qlearning.QLearningAgent(env1)
 
     # Train the agent
-    agent.train(int(5e4))
+    agent.train(int(5e3))
     agent.env.close()
 
     agent.env = env2
     # agent.exploration_prob = 0.0
     print(agent.exploration_prob)
     agent.weighted_actions = True
-    agent.train(2, update_table=False)
+    agent.train(5, update_table=False)
