66a4321fcf4697688cb4434061156d0f828dff1d
Allonte <angelosheltie@gmail.com>
2023-09-04 10:11:45 -0700

    Update base.py

--- dungeonasium/agents/base.py
+++ dungeonasium/agents/base.py
@@ -3,11 +3,14 @@ from gymnasium import vector
 
 class BaseAgent:
     def __init__(self,
-                 env: vector.AsyncVectorEnv | vector.SyncVectorEnv) -> None:
+                 env: vector.AsyncVectorEnv | vector.SyncVectorEnv,
+                 seed: float | None = None) -> None:
         self.env = env
+        self.seed = seed
+        self.env.reset(seed=seed)
 
     @property
-    def render_mode(self):
+    def render_mode(self) -> str:
         """
         Shortcut to render_mode kwarg.
 
@@ -15,3 +18,15 @@ class BaseAgent:
             str: render_mode
         """
         return self.env.spec.kwargs.get("render_mode", None)
+
+    def swap_environments(self,
+                          env: vector.AsyncVectorEnv | vector.SyncVectorEnv) -> None:
+        self.env.close()
+        self.env = env
+        self.env.reset(seed=self.seed)
+
+    def save(self):
+        raise NotImplementedError()
+
+    def load(self):
+        raise NotImplementedError()
